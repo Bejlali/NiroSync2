0ff4f8298a52e98b20c8d96959189b3c104801c6
kingfish60 <babak.ejlali@gmail.com>
2023-07-07 21:37:49 -0400

    107. Creating additional components to route to

---
+++ client/src/app/pubhome/pubhome.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MyhomeComponent } from './myhome.component';
+
+
+
+@NgModule({
+  declarations: [
+    MyhomeComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class PubhomeModule { }
