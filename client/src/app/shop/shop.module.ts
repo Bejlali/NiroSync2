e49a841ab9d284defe4bdfbb87e10e56bda57ce1
kingfish60 <babak.ejlali@gmail.com>
2023-07-13 14:17:41 -0400

    158. Hooking up the product detail component

--- client/src/app/shop/shop.module.ts
+++ client/src/app/shop/shop.module.ts
@@ -5,11 +5,11 @@ import { ProductItemComponent } from './product-item/product-item.component';
 import { SharedModule } from '../shared/shared.module';
 import { ProductDetailsComponent } from './product-details/product-details.component';
 import { ShopRoutingModule } from './shop-routing.module';
+import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [ShopComponent, ProductItemComponent, ProductDetailsComponent],
-  imports: [CommonModule, SharedModule, ShopRoutingModule],
-
+  imports: [CommonModule, SharedModule, ShopRoutingModule, FormsModule],
 })
 export class ShopModule {}
