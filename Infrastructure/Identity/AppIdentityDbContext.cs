00de8a4215e5dc0fa81fdbff554f0bf4290f88fc
kingfish60 <babak.ejlali@gmail.com>
2023-07-14 10:23:22 -0400

    164. Adding the IdentityDbContext

---
+++ Infrastructure/Identity/AppIdentityDbContext.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Identity
+{
+    public class AppIdentityDbContext : IdentityDbContext<AppUser>
+    {
+        public AppIdentityDbContext(DbContextOptions<AppIdentityDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
