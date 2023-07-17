9fad2c519ad57ca8dcf7a989297c82be50455bc6
kingfish60 <babak.ejlali@gmail.com>
2023-07-17 15:52:08 -0400

    194. Persisting the login

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -2,23 +2,42 @@ import { Component, HostBinding, OnInit } from '@angular/core';
 import { AuthService, ScreenService, AppInfoService } from './shared/services';
 import { HttpClient } from '@angular/common/http';
 import { BasketService } from './basket/basket.service';
+import { AccountService } from './account/account.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent  implements OnInit{
+export class AppComponent implements OnInit {
   @HostBinding('class') get getClass() {
-    return Object.keys(this.screen.sizes).filter(cl => this.screen.sizes[cl]).join(' ');
+    return Object.keys(this.screen.sizes)
+      .filter((cl) => this.screen.sizes[cl])
+      .join(' ');
   }
 
-  constructor(private authService: AuthService, private screen: ScreenService, public appInfo: AppInfoService, private http:HttpClient, private BasketService: BasketService ) { }
+  constructor(
+    private authService: AuthService,
+    private screen: ScreenService,
+    public appInfo: AppInfoService,
+    private http: HttpClient,
+    private BasketService: BasketService,
+    private accountService: AccountService
+  ) {}
 
   ngOnInit(): void {
-    const basketId = localStorage.getItem('basket_id')
+    this.loadBasket();
+    this.loadCurrentUser();
+  }
+  loadBasket() {
+    const basketId = localStorage.getItem('basket_id');
     if (basketId) this.BasketService.getBasket(basketId);
-    }
+  }
+
+  loadCurrentUser() {
+    const token = localStorage.getItem('token');
+    if (token) this.accountService.loadCurrentUser(token).subscribe();
+  }
 
   isAuthenticated() {
     return this.authService.loggedIn;
